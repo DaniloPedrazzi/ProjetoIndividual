02711b012d99740418501c07522e4738ab9fbd6b
DaniloPedrazzi <danilo.justino1@gmail.com>
2023-05-14 02:38:23 -0300

    Dinamic header + Leaderboard prototype

---
+++ FrontEnd/header.js
@@ -0,0 +1,13 @@
+document.getElementById("header").innerHTML = 
+`
+    <div class="logo">
+        <h4>Celeste</h4>
+    </div>
+    <ul class="nav-links">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="sobre.html">Sobre</a></li>
+        <li><a href="leaderboard.html">LeaderBoard</a></li>
+        <li><a href="login.html">Login</a></li>
+        <li><a href="cadastro.html">Cadastro</a></li>
+    </ul>
+`;
