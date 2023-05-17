33e2c56bc669bbeb7b3661be586bb1a0d9f32102
DaniloPedrazzi <danilo.justino1@gmail.com>
2023-05-16 23:29:43 -0300

    SignIn/Up + Header Style

--- FrontEnd/header.js
+++ FrontEnd/header.js
@@ -1,13 +1,31 @@
-document.getElementById("header").innerHTML = 
-`
-    <div class="logo">
-        <h4>Celeste</h4>
-    </div>
-    <ul class="nav-links">
-        <li><a href="index.html">Home</a></li>
-        <li><a href="sobre.html">Sobre</a></li>
-        <li><a href="leaderboard.html">LeaderBoard</a></li>
-        <li><a href="login.html">Login</a></li>
-        <li><a href="cadastro.html">Cadastro</a></li>
-    </ul>
-`;
+function carregarHeader(page){
+    var leaderboardSelected = "";
+    var cadastroSelected = "";
+    var loginSelected = "";
+    var dashboardSelected = "";
+    if(page == "leaderboard"){
+        leaderboardSelected = "headerSelected";
+    }else if(page == "cadastro"){
+        cadastroSelected = "headerSelected";
+    }else if(page == "login"){
+        loginSelected = "headerSelected";
+    }else if(page == "dashboard"){
+        dashboardSelected = "headerSelected";
+    }
+    document.getElementById("header").innerHTML =
+    `
+        <div class="logo">
+            <a href="index.html">
+                <img class="logo" src="./assets/img/celesteLogoOnlyMountain.png">
+            </a>
+        </div>
+        <div class="headerCenter">
+            <a class="headerLink" id="${leaderboardSelected}" href="leaderboard.html">LeaderBoard</a>
+            <a class="headerLink" id="${dashboardSelected}" href="dashboard.html">Dashboard</a>
+        </div>
+        <div>
+            <a class="cadastroLink" id="${cadastroSelected}" href="cadastro.html">Cadastro</a>
+            <a class="loginLink" id="${loginSelected}" href="login.html">Login</a>
+        </div>
+    `;
+}
--- FrontEnd/js/funcoes.js
+++ FrontEnd/js/funcoes.js
@@ -1,44 +1,24 @@
 // sessão
 function validarSessao() {
-    // aguardar();
-
     var username = sessionStorage.USERNAME_USUARIO;
     var idUsuario = sessionStorage.ID_USUARIO;
 
-    // var b_usuario = document.getElementById("b_usuario");
-
     if (username != null && idUsuario != null) {
         window.alert(`Seja bem-vindo, ${username}!`);
-        b_usuario.innerHTML = username;
-
-        finalizarAguardar();
     } else {
         window.location = "login.html";
     }
 }
 
 function limparSessao() {
-    // aguardar();
     sessionStorage.clear();
-    // finalizarAguardar();
     window.location = "../login.html";
 }
 
 // carregamento (loading)
 function aguardar() {
-    // var divAguardar = document.getElementById("div_aguardar");
-    // divAguardar.style.display = "flex";
-}
-
-function finalizarAguardar(texto) {
-    // var divAguardar = document.getElementById("div_aguardar");
-    // divAguardar.style.display = "none";
-
-    // var divErrosLogin = document.getElementById("div_erros_login");
-    // if (texto) {
-    //     divErrosLogin.style.display = "flex";
-    //     divErrosLogin.innerHTML = texto;
-    // }
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "flex";
 }
 
 
