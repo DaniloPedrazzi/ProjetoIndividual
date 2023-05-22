a4f4f9aade13f893e932e76cc9b116dcf39d7816
DaniloPedrazzi <danilo.justino1@gmail.com>
2023-05-21 22:54:05 -0300

    Add show password feature

--- FrontEnd/js/funcoes.js
+++ FrontEnd/js/funcoes.js
@@ -8,11 +8,6 @@ function validarSessao() {
     }
 }
 
-function limparSessao() {
-    sessionStorage.clear();
-    window.location = "../login.html";
-}
-
 // carregamento (loading)
 function aguardar() {
     var divAguardar = document.getElementById("div_aguardar");
@@ -24,15 +19,26 @@ function finalizarAguardar() {
     divAguardar.style.display = "none";
 }
 
-
-// modal
-function mostrarModal() {
-    var divModal = document.getElementById("div_modal");
-    divModal.style.display = "flex";
+function ShowPassword(input){
+    var inputSenha = document.getElementById("inputSenha");
+    var inputConfirmarSenha = document.getElementById("inputConfirmarSenha");
+    var eye1 = document.getElementById("eye1");
+    var eye2 = document.getElementById("eye2");
+    if(input == 1){
+        if (inputSenha.type === "password") {
+            inputSenha.type = "text";
+            eye1.style.backgroundImage = "url(../assets/img/hiddenEye.svg)";
+        } else {
+            inputSenha.type = "password";
+            eye1.style.backgroundImage = "url(../assets/img/showingEye.svg)";
+        }
+    }else{
+        if (inputConfirmarSenha.type === "password") {
+            inputConfirmarSenha.type = "text";
+            eye2.style.backgroundImage = "url(../assets/img/hiddenEye.svg)";
+        } else {
+            inputConfirmarSenha.type = "password";
+            eye2.style.backgroundImage = "url(../assets/img/showingEye.svg)";
+        }
+    }
 }
-
-function fecharModal() {
-    var divModal = document.getElementById("div_modal");
-    divModal.style.display = "none";
-}
-
